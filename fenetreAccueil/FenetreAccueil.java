7f404191944f500e018ff3b155f12eda13cd8ad0
Trivy <olivier.gabriel.geom@gmail.com>
2016-10-06 22:17:18 +0200

    Mise en page de la fenêtre d'accueil...

--- fenetreAccueil/FenetreAccueil.java
+++ fenetreAccueil/FenetreAccueil.java
@@ -1,6 +1,7 @@
 package gestionSuivi.fenetreAccueil;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,7 +13,6 @@ import javax.swing.JTable;
 
 import gestionSuivi.data.DataIni;
 import gestionSuivi.data.DataInterf;
-import gestionSuivi.fenetrePlacement.ZModel;
 
 public class FenetreAccueil extends JPanel {
 	private JTable tableauRepartition;
@@ -33,6 +33,9 @@ public class FenetreAccueil extends JPanel {
 	    this.tableauRepartition = new JTable(model);
 	    this.tableauRepartition.setRowHeight(30);
 	    
+	    this.tableauRepartition.getColumn("Euros").setCellRenderer(new NumberRenderer());
+	    this.tableauRepartition.getColumn("%").setCellRenderer(new PercentageRenderer());
+	    
 	    // Bouton et son listener
 	    class MaJListener implements ActionListener{
 			ZModel2 model;
@@ -50,11 +53,15 @@ public class FenetreAccueil extends JPanel {
 	    maJ.addActionListener(new MaJListener(model));
 		
 		// Mise en place finale
+	    JPanel pan = new JPanel();
+	    pan.setPreferredSize(new Dimension(350,100));
+	    pan.add(new JScrollPane(tableauRepartition));
+	    
 		BorderLayout bl = new BorderLayout(); 
 		this.setLayout(bl);
 
 		this.add(new JLabel("Répartition des sommes :"), BorderLayout.NORTH);
-		this.add(new JScrollPane(tableauRepartition), BorderLayout.CENTER);
+		this.add(pan, BorderLayout.CENTER);
 		this.add(maJ,BorderLayout.SOUTH);
 		this.setVisible(true);
 	}
